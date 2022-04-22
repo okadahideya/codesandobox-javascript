a5867e959f205e8006204b2cd189d10cf5e14996
okadahideya <73088476+okadahideya@users.noreply.github.com>
2022-04-22 17:51:53 +0900

    javascript-1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+// var var1 = "Var変数";
+// console.log(var1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var1 = '編集の値';
+// console.log(var1);
+
+// const val4 = {
+//   name: "tanaka",
+//   age: 30,
+// };
+// val4.addres = "jo";
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[2] = "bird";
+// console.log(val5);
+
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2 ;
+// };
+// console.log(func3(10,20));
+
+// 分割代入
+// const myProfile = {
+//   name: "tanaka",
+//   age: 30,
+// }
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 = `名前${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// デフォルト
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// sayHello();
+
+const = nameArr = ["tanaka,"]
