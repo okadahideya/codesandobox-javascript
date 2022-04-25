73b49dfa51553c26907646b1303f613120f884de
okadahideya <73088476+okadahideya@users.noreply.github.com>
2022-04-25 15:14:25 +0900

    javascript-TODO

--- src/index.js
+++ src/index.js
@@ -1,44 +1,88 @@
-// var var1 = "Var変数";
-// console.log(var1);
+const onClickaAdd = () => {
+  // テキストBoxの値取得、初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-// var1 = '編集の値';
-// console.log(var1);
+  createInNolist(inputText);
+};
 
-// const val4 = {
-//   name: "tanaka",
-//   age: 30,
-// };
-// val4.addres = "jo";
-// console.log(val4);
+//未完了list指定の要素を削除
+const deleteFromlist = (target) => {
+  document.getElementById("no-list").removeChild(target);
+};
 
-// const val5 = ["dog", "cat"];
-// val5[2] = "bird";
-// console.log(val5);
+//未完了listに追加する関数
+const createInNolist = (text) => {
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
 
-// function func1(str) {
-//   return str;
-// }
-// console.log(func1("func1です。"));
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = text;
 
-// const func3 = (num1, num2) => {
-//   return num1 + num2 ;
-// };
-// console.log(func3(10,20));
+  // buutonの完了、削除
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //押された完了ボタンの親タグを未完了listから削除
+    deleteFromlist(completeButton.parentNode);
+    // const deleteTarget = completeButton.parentNode;
+    // document.getElementById("no-list").removeChild(deleteTarget);
 
-// 分割代入
-// const myProfile = {
-//   name: "tanaka",
-//   age: 30,
-// }
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`
-// console.log(message1);
+    //完了listに追加する要素
+    const addTarget = completeButton.parentNode;
 
-// const {name, age} = myProfile;
-// const message2 = `名前${name}です。年齢は${age}歳です。`;
-// console.log(message2);
+    //Todo内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
 
-// デフォルト
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
-// sayHello();
+    //div以下を初期化
+    addTarget.textContent = null;
 
-const = nameArr = ["tanaka,"]
+    // liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //戻すボタン
+    const backbutton = document.createElement("button");
+    backbutton.innerText = "戻す";
+    backbutton.addEventListener("click", () => {
+      //押された戻すボタン
+      const deleteTarget = backbutton.parentNode;
+      document.getElementById("yes-list").removeChild(deleteTarget);
+
+      //テクスト取得
+      const text = backbutton.parentNode.firstElementChild.innerText;
+      createInNolist(text);
+    });
+
+    //divダグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backbutton);
+
+    //完了listに追加
+    document.getElementById("yes-list").appendChild(addTarget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグを未完了listから削除
+    deleteFromlist(deleteButton.parentNode);
+    // const deleteTarget = deleteButton.parentNode;
+    // document.getElementById("no-list").removeChild(deleteTarget);
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了のTODO追加
+  document.getElementById("no-list").appendChild(div);
+};
+
+//追加ボタン
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickaAdd());
